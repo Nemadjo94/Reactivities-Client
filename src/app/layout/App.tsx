2e8ed96475ff00a29791ebcb1a33380bc19ef9fd
Nemanja <nemadjo94@gmail.com>
2020-03-23 16:19:52 +0100

    Implemented Error handling

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from '../../features/nav/NavBar';
 import {observer} from 'mobx-react-lite';
-import { Route, withRouter, RouteComponentProps } from 'react-router-dom';
+import { Route, withRouter, RouteComponentProps, Switch } from 'react-router-dom';
 import { HomePage } from '../../features/home/HomePage';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import { ActivityDetails } from '../../features/activities/details/ActivityDetails';
+import { NotFound } from './NotFound';
+import { ToastContainer } from 'react-toastify';
 
 // This is how component looks using react hooks
 const App: React.FC<RouteComponentProps> = ({location}) => {
@@ -24,18 +26,22 @@ const App: React.FC<RouteComponentProps> = ({location}) => {
 
   return (
       <Fragment>
+        <ToastContainer position='bottom-right' />
         <Route exact path='/' component={HomePage} />
         <Route path={'/(.+)'} render={() => (
           <Fragment> 
             <NavBar />
             <Container style={{marginTop: '7em'}}>
-              <Route exact path={`/activities`} component={ActivityDashboard} />
-              <Route exact path={`/activities/:id`} component={ActivityDetails} />
-              <Route 
-                key={location.key}
-                sensitive path={[`/createActivity`, `/manage/:id`]} 
-                component={ActivityForm} 
-              />
+              <Switch>
+                <Route exact path='/activities' component={ActivityDashboard} />
+                <Route path='/activities/:id' component={ActivityDetails} />
+                <Route 
+                  key={location.key}
+                  path={['/createActivity', '/manage/:id']} 
+                  component={ActivityForm} 
+                />
+                <Route path='/notfound' component={NotFound} />
+              </Switch>
             </Container>
           </Fragment>  
         )}/>
--- src/features/activities/details/ActivityDetails.tsx
+++ src/features/activities/details/ActivityDetails.tsx
@@ -16,20 +16,33 @@ interface DetailParams {
 export const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({ match, history }) => {
 
     const activityStore = useContext(ActivityStore);
-    const { activity, loadActivity, loadingInitial } = activityStore;
-    const [loaded, isLoaded] = useState(false); // 
+    const { activity, loadActivity, loadingInitial } = activityStore; // TODO: Vidi zasto loadingInitial ne radi
+    const [loaded, isLoaded] = useState(false);
 
     useEffect(() => {
-        loadActivity(match.params.id).then( // Since load activity is async it goes to detail on second refresh, so I added callback to promise
-            () => isLoaded(true) // Treba staviti da ako ne postoji ta aktivnost da se vrati nazad na activities
-        )                       // Ovako ce samo da ga vrti u krug ako nema ta aktivnost
-    }, [loadActivity, match.params.id])
+        loadActivity(match.params.id)
+        
+        .then( // Since load activity is async it goes to detail on second refresh, so I added callback to promise
+            () => isLoaded(true)
+        )                       
+    }, [loadActivity, match.params.id, history])
 
-    if (loadingInitial || !activity ){
-        return <LoadingComponent content='Loading activity...' />
+    // if (loadingInitial ){ // loading initial zeza. activity radi
+    //     return <LoadingComponent content='Loading activity...' />
+    // }
+
+    // if (!activity){
+    //     return history.push('/notfound');
+    // }
+ 
+    while(loaded === false){
+        return <LoadingComponent content='Loading activity...' /> 
     }
 
-    if (loaded){
+    // if (!activity && loaded) // problem sto uvek vraca na ovo prvo pa tek onda ucita. meni treba loading animacija dok je activity null
+    //     return <NotFound />
+
+    if (activity){
          return (
             <Grid>
                 <Grid.Column width={10}>
@@ -44,7 +57,7 @@ export const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({ m
         )
     }
 
-    return <h1>Error... Something went wrong.</h1> // TODO: make this route back to activities
+    return <LoadingComponent content='Loading activity...' />
 }
 
 export default observer(ActivityDetails);
